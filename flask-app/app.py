83eded7019a9c3ce3b76973d7c1530d811cc7860
John Reagan <jtr4fh@virginia.edu>
2020-06-07 10:30:57 -0400

    flask hello world

---
+++ flask-app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+# A welcome message to test our server
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
